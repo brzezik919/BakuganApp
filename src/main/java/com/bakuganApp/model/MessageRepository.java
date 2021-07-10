4367590db6bcbd059c0a7ede150c899f0e782e41
Patryk <77505092+brzezik919@users.noreply.github.com>
2021-07-10 21:51:07 +0200

    Update and Create new Message Mapping

--- src/main/java/com/bakuganApp/model/MessageRepository.java
+++ src/main/java/com/bakuganApp/model/MessageRepository.java
@@ -1,5 +1,9 @@
 package com.bakuganApp.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface MessageRepository {
     Message save (Message message);
+    Page<Message> findByConversation_IdOrderByTimeDesc(Long id, Pageable pageable);
 }
--- src/main/java/com/bakuganApp/service/MessageService.java
+++ src/main/java/com/bakuganApp/service/MessageService.java
@@ -1,9 +1,7 @@
 package com.bakuganApp.service;
 
 import com.bakuganApp.adapter.SqlMessageRepository;
-import com.bakuganApp.model.Message;
-import com.bakuganApp.model.MessageRepository;
-import com.bakuganApp.model.UserRepository;
+import com.bakuganApp.model.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,22 +14,32 @@ public class MessageService {
     private final MessageRepository messageRepository;
     private final SqlMessageRepository sqlMessageRepository;
     private final UserRepository userRepository;
+    private final ConversationRepository conversationRepository;
+    private final ConversationService conversationService;
 
-    public MessageService(MessageRepository messageRepository, SqlMessageRepository sqlMessageRepository, UserRepository userRepository) {
+    public MessageService(MessageRepository messageRepository, SqlMessageRepository sqlMessageRepository, UserRepository userRepository, ConversationRepository conversationRepository, ConversationService conversationService) {
         this.messageRepository = messageRepository;
         this.sqlMessageRepository = sqlMessageRepository;
         this.userRepository = userRepository;
+        this.conversationRepository = conversationRepository;
+        this.conversationService = conversationService;
     }
 
-    public Page<Message> getListPartnerConversation(int id, int currentPage, int pageSize){
-        Pageable page = PageRequest.of(currentPage, pageSize);
-        return sqlMessageRepository.findUniqueById(id, page);
-    }
-
-    public Message sendMessage(Message toSend, int idWriter, int idReader){
-        toSend.setUserWriter(userRepository.findById(idWriter));
-        toSend.setUserReader(userRepository.findById(idReader));
+    public Message sendMessage(Message toSend, int idFirstUser, int idSecondUser){
+        toSend.setConversation(conversationService.findConversation(idFirstUser, idSecondUser));
+        toSend.setUserWriter(userRepository.findById(idFirstUser));
         toSend.setTime(LocalDateTime.now());
         return messageRepository.save(toSend);
+    }
+
+    public Page<Message> getAllMessagesWithUser(int idFirstUser, int idSecondUser, int currentPage, int pageSize){
+        Pageable page = PageRequest.of(currentPage, pageSize);
+        Conversation conversation = conversationRepository.findByFirstUser_IdAndSecondUser_IdOrFirstUser_IdAndSecondUser_Id(idFirstUser, idSecondUser, idSecondUser, idFirstUser);
+        try{
+            return messageRepository.findByConversation_IdOrderByTimeDesc(conversation.getId(), page);
+        }
+        catch(NullPointerException e){
+            return null;
+        }
     }
 }
